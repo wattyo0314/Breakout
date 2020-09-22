f5c9f9f0b723d65c07e70992c295d3a77703ebe6
wattyo0314 <wantyan-wattyo.0314@outlook.jp>
2020-09-23 01:05:08 +0900

    fixed

--- typinggame/main.js
+++ typinggame/main.js
@@ -1,45 +1,91 @@
 "use strict";
 
 {
+  function setWord() {
+    word = textLists.splice(Math.floor(Math.random() * textLists.length), 1)[0];
+    text.textContent = word;
+  }
   const text = document.getElementById("text");
 
   const textLists = ["javascript", "ruby", "python", "Hello World"];
 
   let checkTexts = [];
-  let loc = 0;
-  let score = 0;
-  let miss = 0;
+  let loc;
+  let score;
+  let miss;
+  let timer;
+  let isPlaying;
+  let timerId;
+  let word;
   const scoreLabel = document.getElementById("score");
   const missLabel = document.getElementById("miss");
+  const timerLabel = document.getElementById("timer");
 
-  createText();
+  function init() {
+    loc = 0;
+    score = 0;
+    miss = 0;
+    timer = 90;
+    text.textContent = "Ready?";
+    scoreLabel.textContent = score;
+    missLabel.textContent = miss;
+    timerLabel.textContent = timer;
+    isPlaying = false;
+  }
 
+  init();
+
+  function updateTimer() {
+    timerId = setTimeout(function () {
+      timer--;
+      timerLabel.textContent = timer;
+      if (timer <= 0) {
+        alert("gameOver");
+        clearTimeout(timerId);
+        return init();
+      }
+      updateTimer();
+    }, 1000);
+  }
+
+  document.addEventListener("click", () => {
+    if (isPlaying === true) {
+      return;
+    }
+    isPlaying = true;
+    setWord();
+    updateTimer();
+  });
   function createText() {
-    const random = Math.floor(Math.random() * textLists.length);
-
     text.textContent = "";
-    checkTexts = String(textLists[random])
+    checkTexts = String(textLists[word])
       .split("")
       .map(function (value) {
         let span = document.createElement("span");
         span.textContent = value;
         text.appendChild(span);
-
+        console.log(value);
         return span;
       });
   }
 
-  document.addEventListener("keydown", keyDown);
-
-  function keyDown(e) {
-    if (e.key === checkTexts[loc].textContent) {
-      checkTexts[loc].className = "add-blue";
-      checkTexts.shift();
-      score++;
-      scoreLabel.textContent = score;
-    } else {
+  document.addEventListener("keydown", (e) => {
+    if (e.key !== word[loc]) {
       miss++;
       missLabel.textContent = miss;
+      return;
     }
-  }
+    loc++;
+    createText();
+    word[loc].className = "add-blue";
+    score++;
+    scoreLabel.textContent = score;
+    if (loc === word.length) {
+      if (word.length === 0) {
+        const elapsedTime = 90 - timer;
+        timerLabel.textContent = `Remaining:${elapsedTime}`;
+      }
+      setWord();
+    }
+  });
 }

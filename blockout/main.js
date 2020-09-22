332d8800181efb2b6b9b2d2436875c604fc9eb59
wattyo0314 <wantyan-wattyo.0314@outlook.jp>
2020-09-22 10:27:39 +0900

    finished

--- blockout/main.js
+++ blockout/main.js
@@ -78,7 +78,7 @@
 
     for (let i = 0; i < level.length; i++) {
       for (let j = 0; j < level[i].length; j++) {
-        if ([i][j]) {
+        if (level[i][j]) {
           block.data.push({
             x: block.width * j,
             y: block.height * i,
@@ -87,16 +87,34 @@
           });
         }
       }
-      console.log(block.data);
     }
   };
 
-  const collide = () => {};
+  const collide = (obj1, obj2) => {
+    return (
+      obj1.x < obj2.x + obj2.width &&
+      obj2.x < obj1.x + obj1.width &&
+      obj1.y < obj2.y + obj2.height &&
+      obj2.y < obj1.y + obj1.height
+    );
+  };
   const loop = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     paddle.update();
     ball.update();
     block.update();
+
+    if (collide(ball, paddle)) {
+      ball.dy *= -1; // なぜ-3にするとブロックが残るのか
+      ball.y = paddle.y - ball.height;
+    }
+
+    block.data.forEach((brick, index) => {
+      if (collide(ball, brick)) {
+        ball.dy *= -1;
+        block.data.splice(index, 10); //2にすると１枚消える時と２枚消える時がある
+      }
+    });
     requestAnimationFrame(loop);
   };
   init();
@@ -110,8 +128,8 @@
     //   paddle.speed = 6;
     // }
     // console.log(e.key); // なんのキーを押したのか
-    if (e.key === "ArrowRight") paddle.speed = -6;
-    if (e.key === "ArrowLeft") paddle.speed = 6;
+    if (e.key === "ArrowRight") paddle.speed = 6;
+    if (e.key === "ArrowLeft") paddle.speed = -6;
   });
   document.addEventListener("keyup", () => (paddle.speed = 0));
 }
